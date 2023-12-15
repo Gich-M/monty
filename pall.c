930ee9e634b7182ef5c372fb49ce981cdbd77a07
Gich-M <gichmuriuki21@gmail.com>
2023-12-15 10:39:45 -0800

    Print all stack values

---
+++ pall.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pall - function that print all the elements in the stack.
+ * @head: Pointer to the stack.
+ * @line_number: Line number of the instruction.
+ * Return: void.
+ */
+void pall(stack_t **head, unsigned int line_number)
+{
+	stack_t *node = *head;
+	(void)line_number;
+
+	while (node != NULL)
+	{
+		fprintf("%d\n", node->n);
+		node = node->next;
+	}
+}
