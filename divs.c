917f20f59b6ae400e8026cbe4ed73cf478f3dd2e
Gich-M <gichmuriuki21@gmail.com>
2023-12-15 03:19:03 -0800

    More calculations

---
+++ divs.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * div - divide second node from top node
+ * @head: top of list
+ * @line_number: line of command
+ */
+void divs(stack_t **head, unsigned int line_number)
+{
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		printf("L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->n == 0 || (*head)->next->n == 0)
+	{
+		printf("L%u: division by zero\n", line_number);
+	}
+	(*head)->next->n /= (*head)->n;
+	(*head) = (*head)->next;
+	free((*head)->prev);
+	(*head)->prev = NULL;
+}
