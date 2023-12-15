c4e679960c4fffa1ee61b3b2536a5575be8e5db8
Gich-M <gichmuriuki21@gmail.com>
2023-12-15 03:14:15 -0800

    Does nothing

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @head: doing nothing
+ * @line_number: no use
+ *
+ * Return: void
+ */
+
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
