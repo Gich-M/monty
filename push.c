acbc925200d4c5bc9d253826c334a4c036bb40a0
Gich-M <gichmuriuki21@gmail.com>
2023-12-15 13:56:55 -0800

    Pushes element to stack

---
+++ push.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+* push - pushes an element to the stack
+* @head: pointer to top of the stack
+* @counter: line_number
+*
+* Return: nothing
+*/
+void push(stack_t **head, unsigned int counter)
+{
+	int i, m = 0, flag = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			m++;
+		for (; bus.arg[m] != '\0'; m++)
+		{
+			if (!isdigit(bus.arg[m]))
+				flag = 1;
+		}
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	i = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, i);
+	else
+		addqueue(head, i);
+}
+
